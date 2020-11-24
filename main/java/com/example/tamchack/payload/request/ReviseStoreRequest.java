6cdd1296e200d1212e51c479ea2c9d4c60a413c2
Yu Jin A <jina6962@naver.com>
2020-11-24 23:27:29 +0900

    [CREATE] revise store info

---
+++ main/java/com/example/tamchack/payload/request/ReviseStoreRequest.java
@@ -0,0 +1,11 @@
+package com.example.tamchack.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviseStoreRequest {
+    private String number;
+    private String openingHours;
+}
