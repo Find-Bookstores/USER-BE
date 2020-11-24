5aaa8e81b2325c5c0a4f1cbe70180597b3fbf5eb
Yu Jin A <jina6962@naver.com>
2020-11-24 23:29:56 +0900

    [CREATE] user login

---
+++ main/java/com/example/tamchack/service/member/auth/UserAuthServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.tamchack.service.member.auth;
+
+import com.example.tamchack.domain.user.User;
+import com.example.tamchack.payload.request.SignInRequest;
+import com.example.tamchack.payload.response.TokenResponse;
+import com.example.tamchack.repository.UserRepository;
+import com.example.tamchack.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserAuthServiceImpl implements UserAuthService {
+
+    private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public TokenResponse UserSignIn(SignInRequest signInRequest) {
+        User user = userRepository.findByIdAndPw(signInRequest.getId(), signInRequest.getPassword());
+        if(user == null){
+            throw new RuntimeException();
+        }
+        String accessToken = jwtUtil.getAccessToken(user.getId());
+        String refreshToken = jwtUtil.getRefreshToken(user.getId());
+        return new TokenResponse(accessToken, refreshToken);
+    }
+}
