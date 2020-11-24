d8e7fb3501b8d66469defb7262f725517ede2de7
Yu Jin A <jina6962@naver.com>
2020-11-24 23:27:41 +0900

    [CREATE] revise store user pw

---
+++ main/java/com/example/tamchack/payload/request/ReviseStoreUserPwRequest.java
@@ -0,0 +1,10 @@
+package com.example.tamchack.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviseStoreUserPwRequest {
+    private String password;
+}
