9fbf3ee9df7b069c0d53e33b387a3724bedcfa77
Yu Jin A <jina6962@naver.com>
2020-11-24 23:31:15 +0900

    [CREATE] application

---
+++ main/java/com/example/tamchack/TamChackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tamchack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamChackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamChackApplication.class, args);
+	}
+
+}
