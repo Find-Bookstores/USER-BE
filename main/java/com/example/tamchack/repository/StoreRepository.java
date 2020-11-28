846178e1c49bb6fa3b5c5d7972f764b4c3a24eaa
Yu Jin A <jina6962@naver.com>
2020-11-28 22:15:11 +0900

    [ADD] search

--- main/java/com/example/tamchack/repository/StoreRepository.java
+++ main/java/com/example/tamchack/repository/StoreRepository.java
@@ -1,6 +1,8 @@
 package com.example.tamchack.repository;
 
 import com.example.tamchack.domain.store.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer>{
     Optional <Store> findByUserId(String userId);
+
+    Page<Store> findAllByStoreNameOrStoreAddressContainsOrderByCreatedAtDesc(
+            String storeName, String Address, Pageable page);
 }
