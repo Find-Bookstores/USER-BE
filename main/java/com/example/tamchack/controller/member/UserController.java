21aa85a763cbb702a8896e02b5cd6647b65d0298
Yu Jin A <jina6962@naver.com>
2020-11-28 22:05:49 +0900

    [REFACTOR] user

--- main/java/com/example/tamchack/controller/member/UserController.java
+++ main/java/com/example/tamchack/controller/member/UserController.java
@@ -1,22 +1,37 @@
 package com.example.tamchack.controller.member;
 
+import com.example.tamchack.payload.request.ReviseUserPwRequest;
+import com.example.tamchack.payload.request.SignInRequest;
 import com.example.tamchack.payload.request.UserSignUpRequest;
+import com.example.tamchack.payload.response.TokenResponse;
+import com.example.tamchack.service.member.auth.UserAuthService;
+import com.example.tamchack.service.member.user.ReviseUserPwService;
 import com.example.tamchack.service.member.user.UserSignUpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/signup/user")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
     private final UserSignUpService userSignUpService;
+    private final UserAuthService userAuthService;
+    private final ReviseUserPwService reviseUserPwService;
 
-    @PostMapping
+    @PostMapping("signup/user")
     public void userSignUp(@RequestBody UserSignUpRequest userSignUpRequest){
 
         userSignUpService.userSignUp(userSignUpRequest);
+    }
+
+    @PostMapping("login/user")
+    public TokenResponse userSignIn(@RequestBody SignInRequest signInRequest, @RequestHeader("Authorization") String Token){
+
+        return userAuthService.userSignIn(signInRequest);
+    }
+
+    @PostMapping("user/mypage/revisepw")
+    public void reviseUserPassword (@RequestBody ReviseUserPwRequest reviseUserPwRequest, @RequestHeader String token) {
+        reviseUserPwService.reviseUserPassword(reviseUserPwRequest, token);
     }
 }
