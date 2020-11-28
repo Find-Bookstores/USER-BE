795828e363cef4fe4672d6b9b3a587aac7a02949
Yu Jin A <jina6962@naver.com>
2020-11-28 22:09:22 +0900

    [CREATE] bookmark

---
+++ main/java/com/example/tamchack/payload/request/BookMarkRequest.java
@@ -0,0 +1,11 @@
+package com.example.tamchack.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookMarkRequest {
+    private String user_id;
+    private String Store_id;
+}
