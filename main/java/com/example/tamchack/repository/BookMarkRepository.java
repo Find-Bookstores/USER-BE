71705f6a04b192676b407c34669b0c78007233bf
Yu Jin A <jina6962@naver.com>
2020-11-28 22:14:45 +0900

    [CREATE] repository

--- main/java/com/example/tamchack/repository/BookMarkRepository.java
+++ main/java/com/example/tamchack/repository/BookMarkRepository.java
@@ -1,9 +1,9 @@
 package com.example.tamchack.repository;
 
-import com.example.tamchack.domain.book.Book;
+import com.example.tamchack.domain.store.BookMark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookMarkRepository extends JpaRepository<BookMark, Integer> {
 }
