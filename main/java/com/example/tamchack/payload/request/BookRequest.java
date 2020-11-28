42fc3d3129977920d98908ecf81bc1836fd24a89
Yu Jin A <jina6962@naver.com>
2020-11-28 22:11:44 +0900

    [CREATE] book info

--- main/java/com/example/tamchack/payload/request/BookRequest.java
+++ main/java/com/example/tamchack/payload/request/BookRequest.java
@@ -1,12 +1,15 @@
 package com.example.tamchack.payload.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
+@Builder
 public class BookRequest {
     private String name;
     private String author;
     private String publisher;
+    private MultipartFile image;
 }
