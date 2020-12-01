4c3ac24f697cac3266dfef6001687ff6542470db
Yu Jin A <jina6962@naver.com>
2020-12-01 13:22:15 +0900

    [ADD] search and recommend method

---
+++ src/main/java/com/example/tamchack/controller/StoreController.java
@@ -0,0 +1,36 @@
+package com.example.tamchack.controller;
+
+import com.example.tamchack.payload.request.BookMarkRequest;
+import com.example.tamchack.payload.request.ReviseStoreRequest;
+import com.example.tamchack.service.book.BookService;
+import com.example.tamchack.service.store.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/store")
+@RequiredArgsConstructor
+public class StoreController {
+    private final StoreService storeService;
+    private final BookService bookService;
+
+    @GetMapping("/search")
+    public void searchStore(@RequestParam String title,
+                           @PageableDefault Pageable pageable){
+        storeService.searchStore(title, pageable);
+    }
+
+    @GetMapping("/searchbook")
+    public void searchBookInStoreInfo(@RequestParam Integer storeId,
+                                      @RequestParam String query,
+                                      @PageableDefault Pageable page){
+        bookService.searchBookInStoreInfo(storeId, query, page);
+    }
+
+    @PutMapping("/recommend")
+    public void recommend(@RequestBody BookMarkRequest bookMarkRequest, @RequestHeader String token){
+        storeService.recommend(bookMarkRequest, token);
+    }
+}
